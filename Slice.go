534dfd637f270a76fe5e19589ab48f4bf69f2360
imac <imac@imacdeiMac.local>
2023-04-08 13:03:35 +0800

    add remove repeat str

---
+++ Slice.go
@@ -0,0 +1,14 @@
+package util4s
+
+// RemoveDuplicateStr 去除重复元素
+func RemoveDuplicateStr(strSlice []string) []string {
+	allKeys := make(map[string]bool)
+	list := []string{}
+	for _, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/r2day/util4s
+
+go 1.20
